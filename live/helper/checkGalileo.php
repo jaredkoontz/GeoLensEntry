7382249fe041a110e5b9767f8b2b2f403ada76ef
koontz <koontz@cs.colostate.edu>
2015-08-04 12:17:49 -0600

    final updates

--- live/helper/checkGalileo.php
+++ live/helper/checkGalileo.php
@@ -22,7 +22,8 @@ function checkGalileo()
 {
     //todo send message to galileo. create and save user id in cookie here.
 
-    $return = $obj = (object)array('status' => 'on', 'url' => 'value', 'id' => '1');
+//    $return = $obj = (object)array('status' => 'on', 'url' => 'value', 'id' => '1');
+    $return = $obj = (object)array('status' => 'off', 'url' => 'value', 'id' => '1');
     echo json_encode($return);
 }
 
